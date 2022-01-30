46eb67f640ff78095a83ae7800ac3f24a67f3c76
andrea.montoya@ed.company <terceiro-aruiz@icatuseguros.com.br>
2022-01-30 12:46:30 -0300

    feat: adicionando componente search

---
+++ src/app/pages/weather/weather.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'amr-weather',
+  templateUrl: './weather.component.html',
+  styleUrls: ['./weather.component.scss']
+})
+export class WeatherComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
